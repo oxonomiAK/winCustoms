415c6cc6b9265deaab3469a3843c2344ffbecdf1
oxonomii <grejs1704@gmail.com>
2024-11-25 18:31:16 +0200

    theme  test 0.0.1

--- src/main/java/com/customizer/Main.java
+++ src/main/java/com/customizer/Main.java
@@ -3,6 +3,8 @@ package com.customizer;
 import com.customizer.core.dwTemp;
 import com.customizer.features.wallpapers.WallpaperManager;
 import com.customizer.core.PlatformVersionInfo;
+import com.customizer.core.acTemp;
+import com.customizer.features.themes.ThemeManager;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -11,5 +13,8 @@ public class Main {
         System.out.print("Path of default wallpaper: ");
         System.out.println(dwTemp.defaultWallpaper);
         PlatformVersionInfo.PlatformInfo();
+        ThemeManager.getAccentColorFromWinIni();
+        System.out.print("Accent Color Hex: ");
+        System.out.println(acTemp.defaultAccentColor);
     }
 }
---
+++ src/main/java/com/customizer/core/acTemp.java
@@ -0,0 +1,5 @@
+package com.customizer.core;
+
+public class acTemp {
+    public static long defaultAccentColor;
+}
